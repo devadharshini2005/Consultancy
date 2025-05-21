c9df8c1a5dad0c46e3917fa136bef293cb17fa98
devadharshini <devadharshinit.22aid@kongu.edu>
2025-05-21 07:36:55 +0530

    6  commit

--- ecom-cloth-main/ecom-cloth-main/admin/src/App.jsx
+++ ecom-cloth-main/ecom-cloth-main/admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = 'https://consultancy-fbur.onrender.com';
+export const backendUrl = 'https://consultancy-fbur.onrender.com/';
 
 console.log("Backend URL:", backendUrl);
 export const currency = '₹';
